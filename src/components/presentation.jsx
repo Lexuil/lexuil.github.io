4371197b15652ad78b963de64473c7fd0b1dbed9
Lexuil <ojumanab@unal.edu.co>
2023-05-28 19:41:40 -0500

    add projects view

--- src/components/presentation.jsx
+++ src/components/presentation.jsx
@@ -3,19 +3,29 @@ import Link from 'next/link'
 import { m } from 'framer-motion'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import { IoMdMail } from 'react-icons/io'
+import { useContext } from 'react'
+import { ViewContext } from '@/context'
 
 export default function Presentation() {
+  const setViewProjects = useContext(ViewContext)
+
   return (
     <m.section
-      className='flex flex-col items-center justify-center'
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 2 }}
+      className='flex flex-col items-center justify-center min-h-screen'
+      initial={{ display: 'none', opacity: 0 }}
+      animate={{
+        display: 'flex',
+        opacity: 1,
+        transition: { duration: 1, delay: 1 }
+      }}
+      exit={{
+        opacity: 0,
+        x: 700,
+        transition: { duration: 1 }
+      }}
     >
       <p className='font-medium'>HOLA, YO SOY</p>
-      <h1 className='text-3xl sm:text-7xl font-extrabold sm:font-black text-center whitespace-pre-line'>{`Oscar Julian
-      Umaña Benavides
-      `}</h1>
+      <h1 className='text-3xl sm:text-7xl font-extrabold sm:font-black text-center whitespace-pre-line'>Oscar Umaña</h1>
 
       <Image src='/oscar.jpg' width={200} height={200} alt='' className='rounded-full my-5'/>
 
@@ -33,8 +43,12 @@ export default function Presentation() {
 
       <p className='text-xl font-medium'>Desarrollador web</p>
       <p className=''>Estudiante de ingeniería de electrónica</p>
-
-      <button className='bg-white bg-opacity-10 border rounded-lg text-white px-5 py-2 mt-5 font-medium'>Proyectos</button>
+      <button
+        className='bg-white bg-opacity-10 border rounded-lg text-white px-5 py-2 mt-5 font-medium'
+        onClick={() => setViewProjects(true)}
+      >
+        Proyectos
+      </button>
     </m.section>
   )
 }

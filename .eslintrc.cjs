edf78ea916456e4f613984e74048bded825dde54
Lexuil <ojumanab@unal.edu.co>
2023-09-10 19:50:01 -0500

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "plugin:astro/all",
+    "plugin:jsx-a11y/recommended"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+        "astro/semi": "off"
+      }
+    }
+  ]
+};

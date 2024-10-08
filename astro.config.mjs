a7b42cb3406a6949274f30aaec130d2fb4b79d5d
Lexuil <ojumanab@unal.edu.co>
2024-10-07 22:58:41 -0500

    Use sharp instead squoosh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-import {
-  defineConfig,
-  squooshImageService
-} from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: squooshImageService(),
-  },
   integrations: [tailwind(), icon()]
 });
